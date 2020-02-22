2e1f38ba4ee1a33be6b093a3e9d9258a13003dfa
Osmar Leandro <osmarleandrods@gmail.com>
2020-02-22 15:19:15 -0300

    [ref] Inline method at line 34 IoUtils#extracted()

--- src/main/java/org/springframework/build/aws/maven/IoUtils.java
+++ src/main/java/org/springframework/build/aws/maven/IoUtils.java
@@ -31,10 +31,6 @@ final class IoUtils {
     static void copy(InputStream in, OutputStream out) throws IOException {
         byte[] buffer = new byte[BUFFER_SIZE];
 
-        extracted(in, out, buffer);
-    }
-
-    private static void extracted(InputStream in, OutputStream out, byte[] buffer) {
         int read;
 
         try {
